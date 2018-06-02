09386b6392070af8b3c8b55fce9766f0bd68515f
原田 拓磨 <t.harada@april-knights.com>
2018-06-02 17:25:00 +0900

    Vueとfirebaseを使ったお遊び googleアカウントのログイン機能

---
+++ src/App.vue
@@ -0,0 +1,24 @@
+<template>
+  <div id="app">
+    <Home v-if="!isLogin"></Home>
+    <Editor v-if="isLogin"></Editor>
+  </div>
+</template>
+
+<script>
+import Home from './components/Home.vue';
+import Editor from './components/Editor.vue';
+
+export default{
+  name:'app',
+  data(){
+    return{
+      isLogin:false
+    }
+  },
+  components:{
+    'Home':Home,
+    'Editor':Editor,
+  },
+}
+</script>
